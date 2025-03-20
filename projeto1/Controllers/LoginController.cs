16eede3ceb3b50a72eacd26afd4375dfcd07ae8a
MarcoSpecian <marco.specian94@gmail.com>
2025-03-20 15:13:52 -0300

    construindo a model usuario

--- projeto1/Controllers/LoginController.cs
+++ projeto1/Controllers/LoginController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using projeto1.Repositorio;
+using projeto1.Models;
 
 namespace projeto1.Controllers
 {
     public class LoginController : Controller
     {
+        //contrutor
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
         public IActionResult Login()
         {
             return View();
@@ -19,6 +27,20 @@ namespace projeto1.Controllers
             }
             ModelState.AddModelError("", "Email ou senha inválidos");
             return View();
+
+        }
+        public IActionResult Cadastro() {
+            return View();    
+        }
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if(ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");   
+            }
+            return View(usuario);
         }
     }
 }
