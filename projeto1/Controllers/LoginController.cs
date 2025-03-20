ad1c4a11f8e5f4a3533d53c0a066bee845d7f4be
MarcoSpecian <marco.specian94@gmail.com>
2025-03-20 14:40:26 -0300

    criando a view Login

--- projeto1/Controllers/LoginController.cs
+++ projeto1/Controllers/LoginController.cs
@@ -4,7 +4,7 @@ namespace projeto1.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
